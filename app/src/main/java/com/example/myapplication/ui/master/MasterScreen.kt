cd9b2456db5dc5b73508a8b7dd10c7300ec63429
dangtu <dang.huu.tu88@gmail.com>
2024-04-27 20:43:38 +0900

    hide/show bottomBar on Screen

--- app/src/main/java/com/example/myapplication/ui/master/MasterScreen.kt
+++ app/src/main/java/com/example/myapplication/ui/master/MasterScreen.kt
@@ -1,22 +1,13 @@
 package com.example.myapplication.ui.master
 
-import android.content.Intent
-import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.ThumbUp
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -25,18 +16,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-import com.example.myapplication.R
-import com.example.myapplication.common.Constants
 import com.example.myapplication.ui.navigation.AppNavigationActions
 import com.example.myapplication.ui.navigation.AppNavigationGraph
 import com.example.myapplication.ui.navigation.Screen
 import com.example.myapplication.ui.theme.MyApplicationTheme
-
 
 @Composable
 fun MasterScreen(
@@ -46,9 +33,9 @@ fun MasterScreen(
 ) {
     // store top bar state
     var appBarState by remember {
-        mutableStateOf(TopAppBarState(Icons.Default.Home, TopBarTitle.Home.toString()))
+        mutableStateOf(TopAppBarState(Icons.Filled.Home, TopBarTitle.Home.toString()))
     }
-    // store the selected state
+    // store the selected state cross configuration change
     val selectedItemIndex by rememberSaveable {
         mutableStateOf(0)
     }
@@ -59,33 +46,50 @@ fun MasterScreen(
     val navActions = remember(navController) {
         AppNavigationActions(navController)
     }
+    // bottom bar state
+    var bottomBarState by rememberSaveable {
+        mutableStateOf(true)
+    }
+
     Scaffold(
         topBar = { buildTopAppBar(navActions, appBarState) },
         bottomBar = {
-            buildBottomBar(items, selectedItemIndex, navActions, favBadge)
+            if (bottomBarState)
+                buildBottomBar(items, selectedItemIndex, navActions, favBadge)
         }
     ) {
         Column(modifier = Modifier.padding(it)) {
             MasterScreenContent(it, navController, navActions) { destination ->
                 when (destination) {
-                    Screen.HomeScreen.route ->
+                    Screen.HomeScreen.route -> {
+                        // top bar
                         appBarState = appBarState.copy(
                             navIcon = Icons.Filled.Home,
                             title = TopBarTitle.Home.toString()
                         )
+                        // show bottom bar
+                        bottomBarState = true
+                    }
 
-                    Screen.FavouriteScreen.route ->
+                    Screen.FavouriteScreen.route -> {
                         appBarState = appBarState.copy(
                             navIcon = Icons.Filled.ThumbUp,
                             title = TopBarTitle.Favourites.toString()
                         )
+                        // show bottom bar
+                        bottomBarState = true
+                    }
 
-                    else ->
+                    else -> {
                         appBarState = appBarState.copy(
                             navIcon = Icons.AutoMirrored.Filled.ArrowBack,
                             title = TopBarTitle.Details.toString(),
                             hasActionMenu = ActionMenuType.ShareMenu
                         )
+                        // hide bottom bar
+                        bottomBarState = false
+                    }
+
 
                 }
             }
