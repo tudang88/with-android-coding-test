1455cf708b9e14eecf1d545915695ee55be13e94
dangtu <dang.huu.tu88@gmail.com>
2024-04-28 21:36:18 +0900

    remove unused import

--- app/src/main/java/com/example/myapplication/ui/navigation/AppNavigationActions.kt
+++ app/src/main/java/com/example/myapplication/ui/navigation/AppNavigationActions.kt
@@ -1,6 +1,5 @@
 package com.example.myapplication.ui.navigation
 
-import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 
 object AppNavigationArgs {
