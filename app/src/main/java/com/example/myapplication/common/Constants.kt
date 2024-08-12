c085d8343ff45bea068ca477a58ac590755e9db0
dangtu <dang.huu.tu88@gmail.com>
2024-08-12 15:46:19 +0900

    update API BASE_URL

--- app/src/main/java/com/example/myapplication/common/Constants.kt
+++ app/src/main/java/com/example/myapplication/common/Constants.kt
@@ -2,7 +2,7 @@ package com.example.myapplication.common
 
 object Constants {
     const val LOG_TAG_COMMON = "MyAppLog"
-    const val BASE_URL = "https://raw.githubusercontent.com/withjp-inc/with_android_coding_test/main/"
+    const val BASE_URL = "https://raw.githubusercontent.com/tudang88/with-android-coding-test/main/"
     const val USER_PROFILE_ENDPOINT = "api/users/users.json"
     const val DB_NAME = "local_database"
 }
